30dcc0ef47b8e9460c7088cbd1298c99610c81b3
Paul Boonyarungsrit <tboonyar@gmail.com>
2026-05-06 21:34:30 +1000

    fix(web): scale icons to 1em (match adjacent text height)

--- apps/web/src/components/icon.tsx
+++ apps/web/src/components/icon.tsx
@@ -7,10 +7,7 @@ interface Props {
 
 export function Icon({ name, className }: Props) {
   return (
-    <span
-      aria-hidden="true"
-      className={cn("material-symbols-outlined translate-y-[1px] text-[1.1em]", className)}
-    >
+    <span aria-hidden="true" className={cn("material-symbols-outlined text-[1em]", className)}>
       {name}
     </span>
   );
